c8a6bc35ca40285f96ce9e20da234ef90d442a66
priya-06-kumari <priyaguptaaaa0604@gmail.com>
2026-05-31 14:35:39 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -10,19 +10,16 @@ import com.example.myrecyclerview.model.Paintings
 class MainActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
 
-    var data = mutableListOf<Paintings>()  //used to create an array
+    var data = mutableListOf<Paintings>() 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        recyclerView = findViewById(R.id.rec) //now we have to provide recycler manager  to recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this) //layout manager is a function this is used to points current activity object or current scree;s context
+        recyclerView = findViewById(R.id.rec) 
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
-//        var paint = Paintings(R.drawable.c , "Mithila" , "32")
-//        data.add(paint) ->it is for understanding only
 
-        //object creation of Paintings class in data array and we assign values here
         data.add(Paintings(R.drawable.a , "Mithila Painting" , "Traditional Art"))
         data.add(Paintings(R.drawable.b , "Girl Painting", "Traditional Art"))
         data.add(Paintings(R.drawable.c , "Women Art" , "Traditional Art"))
